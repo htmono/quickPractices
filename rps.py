cb315ef401dbececda0b78c503701d1f6a6ea416
htmono <heikkimononen@gmail.com>
2022-10-13 08:06:28 +0300

    Cleaning the code and what user sees (added line of "-" between games".

--- rps.py
+++ rps.py
@@ -8,34 +8,34 @@ moves = ["rock", "paper", "scissors"]
 while True:
 
     # make variable for players pick and ask player for input
-    pcChoose = input("Which do you choose? (rock/paper/scissors \nIf you want to quit, you can type in 'quit' or 'q' ")
+    playerChose = input("Which do you choose? (rock/paper/scissors \nIf you want to quit, you can type in 'quit' or 'q' ")
 
     # make variable for computer to pick
-    opChoose = random.choice(moves)
+    computerChose = random.choice(moves)
 
     # Player can quit the game by typing out "quit" or "q"
-    if pcChoose == "quit" or pcChoose == "q":
+    if playerChose == "quit" or playerChose == "q":
         break
     # deal with finding out the winner
-    print(f"You chose {pcChoose} and computer chose {opChoose}")
-    if pcChoose == opChoose:
+    print(f"You chose {playerChose} and computer chose {computerChose}")
+    if playerChose == computerChose:
         print("It's a tie!")
-    if pcChoose == "rock":
-        if opChoose == "paper":
+    if playerChose == "rock":
+        if computerChose == "paper":
             print("Computer won!")
-        elif opChoose == "scissors":
+        elif computerChose == "scissors":
             print("You won!")
 
-    if pcChoose == "paper":
-        if opChoose == "rock":
+    if playerChose == "paper":
+        if computerChose == "rock":
             print("You won!")
-        elif opChoose == "scissors":
+        elif computerChose == "scissors":
             print("Computer won!")
 
-    if pcChoose == "scissors":
-        if opChoose == "rock":
+    if playerChose == "scissors":
+        if computerChose == "rock":
             print("Computer won!")
-        elif opChoose == "paper":
+        elif computerChose == "paper":
             print("You won!")
     # dotted lines for visibility
     print("-" * 10)
