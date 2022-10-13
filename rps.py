9fa2999284c8fed89a80d9e2c6a8750ed9eb5bd1
htmono <heikkimononen@gmail.com>
2022-10-13 08:20:36 +0300

    implemented score to check who won most games.

--- rps.py
+++ rps.py
@@ -4,11 +4,17 @@ import random
 # Make list out of the possible choices player and computer can make
 moves = ["rock", "paper", "scissors"]
 
+playerScore = 0
+computerScore = 0
+
+
 # Set up a while-loop for game to continue as long as player wants it.
 while True:
 
     # make variable for players pick and ask player for input
-    playerChose = input("Which do you choose? (rock/paper/scissors \nIf you want to quit, you can type in 'quit' or 'q' ")
+    print("If you want to quit, you can type in 'quit' or 'q' ")
+    playerChose = input("Which do you choose? (rock/paper/scissors ")
+
 
     # make variable for computer to pick
     computerChose = random.choice(moves)
@@ -23,19 +29,38 @@ while True:
     if playerChose == "rock":
         if computerChose == "paper":
             print("Computer won!")
+            computerScore += 1
         elif computerChose == "scissors":
             print("You won!")
+            playerScore += 1
 
     if playerChose == "paper":
         if computerChose == "rock":
             print("You won!")
+            playerScore += 1
         elif computerChose == "scissors":
             print("Computer won!")
+            computerScore += 1
 
     if playerChose == "scissors":
         if computerChose == "rock":
             print("Computer won!")
+            computerScore += 1
         elif computerChose == "paper":
             print("You won!")
+            playerScore += 1
     # dotted lines for visibility
     print("-" * 10)
+
+# Check who won the game
+if playerScore > computerScore:
+    winner = "You"
+elif playerScore < computerScore:
+    winner = "Computer"
+else:
+    winner = 0
+
+if winner == 0:
+    print (f"You got {playerScore} points and computer got {computerScore} points. It's a tie!")
+else:
+    print(f"You got {playerScore} points and computer got {computerScore} points. {winner} won!")
